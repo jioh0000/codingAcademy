b1c24bbc7519e9b6834affe785211e760eb33839
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-03-11 00:01:31 +0900

    Update dailyWork.py

--- Lecture/dailyWork.py
+++ Lecture/dailyWork.py
@@ -1,13 +1,15 @@
 #3/10
-print("Input: ")
+print("Input: ", end="")
 a = int(input())
 
-for i in range(2,a+1):
+if a>=2:
+        print(2," ",end="")
+
+for i in range(3,a+1):
     b=0
-    if a==2:
-        print(a," ")
-    for j in range(2,a):
+    for j in range(2,i):
         if i%j==0:
             b=1
     if b==0:
-        print(i," ")
+        print(i," ",end="")
+
