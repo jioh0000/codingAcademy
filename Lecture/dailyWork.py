8b8ad20b4348957a334c0d0a3fafa7c859cef6c8
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-03-13 11:55:25 +0900

    Update dailyWork.py

--- Lecture/dailyWork.py
+++ Lecture/dailyWork.py
@@ -32,10 +32,23 @@ for i in range(0,11):
 
 #3/12
 """
-
-"""
 a=0
 for i in range(0,4):
     for j in range(0,4):
         print(j+i,end=" ")
     print()
+"""
+
+#3/13
+"""
+
+"""
+low_prices = [100,200,400,800,1000]
+high_prices = [150,300,430,880,1000]
+volatility = []
+
+for i in range(0,len(low_prices)):
+    volatility.append(high_prices[i]-low_prices[i])
+
+for i in range(0,5):
+    print(volatility[i], end=" ")
