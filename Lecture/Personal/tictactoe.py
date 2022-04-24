45644a2d64a56f4a0f8079a71660599a33257168
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-04-24 11:31:05 +0900

    Update tictactoe.py

--- Lecture/Personal/tictactoe.py
+++ Lecture/Personal/tictactoe.py
@@ -1,93 +1,73 @@
-from sympy import false
+def check_result(player_choice, current_player):
 
+    winningGridPosition = [[7,8,9],[4,5,6],[1,2,3],[1,5,9], 
+                [3,5,7],[1,4,7],[2,5,8],[3,6,9]] #Cases in which determine a winner
 
-def grid_tic_tac_toe(inputs):
-    print("\n")
-    print(f"\t [{inputs[0]}] | [{inputs[1]}] | [{inputs[2]}]\n")
-    print(f"\t [{inputs[3]}] | [{inputs[4]}] | [{inputs[5]}]\n")
-    print(f"\t [{inputs[6]}] | [{inputs[7]}] | [{inputs[8]}]\n")
+    for i in winningGridPosition:
+        if all(j in player_choice[current_player] for j in i):
+            # If any winning combi satisfies, return win
+            return 'Win'
 
-
-
-#print(grid_tic_tac_toe(['X','O','X','X','O','X','X','O','X']))
-
-#Function to check if anyone has won
-def check_result(player_position, current_player):
-
-    if len(player_position['X']) + len(player_position['O']) == 9:
-        return "Draw"
-
-#이길 수 있는 모든 경우의 수
-    solution = [[1,2,3], [4,5,6], [7,8,9], [1,5,9], 
-                [3,5,7], [1,4,7], [2,5,8], [3,6,9]]
-    
-    for i in solution:
-        if all(y in player_position[current_player] for y in i):
-            # if any_winning combi satisfies, return true
-            return "Win"
+    if len(player_choice['X']) + len(player_choice['O']) == 9:
+        return 'Draw'
 
     return False
 
 
-def single_game(current_player):
-    inputs = []
-    for i in range(9):
-        inputs.append(' ')
-    #stores the positions of X and O
-    player_position = {'X':[], 'O':[]}
-    while True:
-        grid_tic_tac_toe(inputs)
-    
-        print("Player", current_player, "Select a grid!:", end=" ")
-        move = int(input())
-        
-        if move < 1 or move > 9:
-            print()
-            continue
-        
-        
-        if inputs[move - 1] != ' ':
-            print("grid alread taken. Try again")
-            continue
-        
-        inputs[move - 1] = current_player
-        player_position[current_player].append(move)
-
-        #Function call for check_win
-        if check_result(player_position, current_player) == "Win":
-            grid_tic_tac_toe(inputs)
-            print("Player ", current_player, "won\n")
-            return current_player
-
-        if check_result(player_position, current_player) == "Draw":
-            grid_tic_tac_toe(inputs)
-            print("Game Drawn\n")
-            return "Drawn"
-
-        #switch player
-        if current_player == 'X':
-            current_player = 'O'
-        else:
-            current_player = 'X'
-
-
-#main
-player1 = "X"
-player2 = "O"
-
+player1 = 'X'
+player2 = 'O'
 current_player = player1
-player_choice = {'X': "", 'O': ""}
 
 while True:
-    player_choice['X'] == current_player
-    if current_player == player1:
-        player_choice['O'] = player2
-    else:
-        player_choice['O'] = player1
 
-    single_game('X')
+    inputlist = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
-    if current_player == player1:
-        current_player = player2
-    else:
-        current_player = player1
+    #Stores the positions of X and O
+
+    player_choice = {'X':[], 'O':[]}
+    
+    while True:
+        #Output (Grid)
+        print("\n")
+        print(f"\t [{inputlist[0]}] | [{inputlist[1]}] | [{inputlist[2]}]\n") 
+        print(f"\t [{inputlist[3]}] | [{inputlist[4]}] | [{inputlist[5]}]\n")
+        print(f"\t [{inputlist[6]}] | [{inputlist[7]}] | [{inputlist[8]}]\n")
+    
+        print(current_player, "--> Select a grid!:", end=" ")
+        #Input (GridNum)
+        userSelectGrid = int(input())
+        
+        if inputlist[userSelectGrid - 1] != ' ':
+            print("Alread taken. Try again")
+            continue
+        
+        inputlist[userSelectGrid - 1] = current_player
+
+
+        player_choice[current_player].append(userSelectGrid)
+
+
+        #Function call for check win
+        if check_result(player_choice, current_player) == 'Win':
+            #Output (Grid)
+            print("\n")
+            print(f"\t [{inputlist[0]}] | [{inputlist[1]}] | [{inputlist[2]}]\n")
+            print(f"\t [{inputlist[3]}] | [{inputlist[4]}] | [{inputlist[5]}]\n")
+            print(f"\t [{inputlist[6]}] | [{inputlist[7]}] | [{inputlist[8]}]\n")
+            print('winner is ',current_player,'\n')
+            break
+        #Function call for check draw
+        if check_result(player_choice, current_player) == "Draw":
+            #Output (Grid)
+            print("\n")
+            print(f"\t [{inputlist[0]}] | [{inputlist[1]}] | [{inputlist[2]}]\n")
+            print(f"\t [{inputlist[3]}] | [{inputlist[4]}] | [{inputlist[5]}]\n")
+            print(f"\t [{inputlist[6]}] | [{inputlist[7]}] | [{inputlist[8]}]\n")
+            print('Draw\n')
+            break
+
+        #switch player
+        if current_player == 'O':
+            current_player = 'X'
+        else:
+            current_player = 'O'
